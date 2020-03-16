92f9eb431d3c46381e0dde6e53a20e35dc718432
ahmedmrashad96 <ahmedmrashad96@gmail.com>
2020-03-16 19:27:16 +0200

    .

---
+++ src/Debug/TEST/TEST.d
@@ -0,0 +1,15 @@
+TEST/TEST.d TEST/TEST.o: ../TEST/TEST.c ../TEST/TEST_LOCAL.h \
+  ../TEST/../COMMON/Std_Types.h ../TEST/../COMMON/Utils.h \
+  ../TEST/../DIO/DIO.h ../TEST/../DIO/DIO_CFG.h ../TEST/TEST_CFG.h
+
+../TEST/TEST_LOCAL.h:
+
+../TEST/../COMMON/Std_Types.h:
+
+../TEST/../COMMON/Utils.h:
+
+../TEST/../DIO/DIO.h:
+
+../TEST/../DIO/DIO_CFG.h:
+
+../TEST/TEST_CFG.h:
---
+++ src/Debug/main.d
@@ -0,0 +1,5 @@
+main.d main.o: ../main.c ../TEST/TEST.h ../TEST/TEST_CFG.h
+
+../TEST/TEST.h:
+
+../TEST/TEST_CFG.h:
